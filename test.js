36ad85219dc728e74b250e143954891b41bf8c13
Simen Li <simenkid@gmail.com>
2015-11-13 14:24:56 +0800

    test

--- test.js
+++ test.js
@@ -1,4 +1,24 @@
-let numbers = [1, 2, 3];
-let [first, , third] = numbers;
+var _ = require('lodash');
 
-console.log(first);
+function Foo() {
+  this.a = 1;
+  this.b = 2;
+  this.c = {
+  	c1: 1,
+  	c2: 'c2',
+  	c3: ['xx', 'yy'],
+  	c4: function () {}
+  };
+  this.x = function () {};
+  this.y = function () {};
+}
+
+Foo.prototype.d = 3;
+Foo.prototype.e = function () {};
+
+var result = {};
+_.forOwn(new Foo(), function(value, key) {
+	if (!_.isFunction(value)) 
+});
+
+console.log(_.toPlainObject(new Foo()));
