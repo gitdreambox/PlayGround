d2fe1096d8c4185f456d37f8889fcb13c90e8e76
Simen Li <simenkid@gmail.com>
2015-12-13 13:51:23 +0800

    demo code

--- http_demo/app.js
+++ http_demo/app.js
@@ -1,6 +1,8 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs'),
+    idxFilePath = __dirname + '/index.html';
 
 http.createServer(function (req, res) {
-	res.writeHead(200, { 'Content-Type': 'text/plain'});
-	res.send('Hello World!\n');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream(idxFilePath).pipe(res);
 }).listen(3456, '127.0.0.1');
