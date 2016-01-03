964cfc11c163ab3711c35c87870b22b4ae5b0f5b
Simen Li <simenkid@gmail.com>
2016-01-03 11:50:55 +0800

    demo

--- http_demo/app.js
+++ http_demo/app.js
@@ -3,6 +3,16 @@ var http = require('http'),
     idxFilePath = __dirname + '/index.html';
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.createReadStream(idxFilePath).pipe(res);
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream(idxFilePath).pipe(res);
+    } else if (req.url === '/api') {
+        var obj = { firstname: 'Simen', lastname: 'Li' };
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(obj));
+    } else {
+        res.writeHead(404);
+        res.end('<h1>404 Not Found<h1>');
+    }
+
 }).listen(3456, '127.0.0.1');
